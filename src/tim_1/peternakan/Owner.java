8f047cda862593481818f9a33ca45cb3020b75b1
AndiAq06 <ardiansyahnasir56@gmail.com>
2023-05-26 15:06:08 +0700

    first commit from branch andi

---
+++ src/tim_1/peternakan/Owner.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tim_1.peternakan;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Owner {
+    private String nama;
+    private String email;
+    private String password;
+    private String address;
+    private String contactNumber;
+    
+    
+    public void beliHewan(){
+                System.out.println("Owner membeli hewan");
+           
+    }
+    
+        public void jualHewan(){
+                System.out.println("Owner menjual  hewan");
+           
+    }
+}
